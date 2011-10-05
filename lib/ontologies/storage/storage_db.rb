8a26c728c1fe25c2e43fb72c9dcf9b53906fdd44
deil <deil@mneko.net>
2011-10-05 21:16:34 +0400

    xen: DomUConfig class for storing domU configurations

--- lib/ontologies/storage/storage_db.rb
+++ lib/ontologies/storage/storage_db.rb
@@ -1,5 +1,5 @@
-require 'activesupport'
-require 'activerecord'
+require 'active_support'
+require 'active_record'
 
 class KnownFact < ActiveRecord::Base
   named_scope :current, :conditions => {:is_active => 1}
@@ -84,17 +84,3 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
   :database => "#{AGENT_ROOT}/databases/storage.sqlite"
 )
-
-class AgentState
-  def self.export_should_be_up?(disk_number)
-    state = VirtualDiskState.find_by_disk_number(disk_number)
-    return state && state.is_up == true ? true : false
-  end
-  
-  def self.update_export_state(disk_number, is_up)
-    state = VirtualDiskState.find_by_disk_number(disk_number)
-    state = VirtualDiskState.new(disk_number, is_up) unless state
-    state.is_up = is_up
-    state.save
-  end
-end
--- lib/ontologies/xen/xen_db.rb
+++ lib/ontologies/xen/xen_db.rb
@@ -1,5 +1,5 @@
-require 'activesupport'
-require 'activerecord'
+require 'active_support'
+require 'active_record'
 
 class KnownFact < ActiveRecord::Base
   named_scope :current, :conditions => {:is_active => 1}
@@ -47,35 +47,66 @@ class VirtualDisk
 
 end
 
-class VirtualDiskState
-  attr_reader :disk_number
-  attr_accessor :is_up
-  
-  def initialize(disk_number, is_up)
-    @disk_number = disk_number
-    @is_up = is_up
+class DomUConfig
+  attr_reader :name
+  attr_accessor :is_hvm
+  attr_accessor :ram
+  attr_accessor :vcpus
+  attr_accessor :cpu_weight
+  attr_accessor :cpu_cap
+  attr_accessor :disks
+  attr_accessor :eth0_mac
+  attr_accessor :eth1_mac
+  attr_accessor :vnc_port
+  attr_accessor :boot_device
+
+  def initialize(name)
+    @name = name
   end
 
-  def self.find_by_disk_number(disk_number)
-    fact = KnownFact.current.find_by_key('vd' + disk_number.to_s + '-state')
-    return nil unless fact
-    VirtualDiskState.new(disk_number, fact.value == 'up')
-  end
-  
   def save(origin = nil, agent = nil)
-    fact = KnownFact.current.find_by_key('vd' + @disk_number.to_s + '-state')
-    fact = KnownFact.new(:key => 'vd' + @disk_number.to_s + '-state', :is_active => 1) unless fact
-    fact.value = @is_up ? 'up' : 'down'
+    fact = KnownFact.current.find_by_key('domu_' + self.name)
+    fact = KnownFact.new(:key => 'domu_' + self.name, :is_active => 1) unless fact
+    fact.value = self.to_json
     fact.origin = origin
     fact.agent = agent
     fact.save
   end
 
-  def delete
-    fact = KnownFact.current.find_by_key('vd' + @disk_number.to_s + '-state')
+  def delete()
+    fact = KnownFact.current.find_by_key('domu_' + self.name)
     fact.update_attributes(:is_active => false) if fact
   end
-  
+
+  def self.all
+    domus = []
+    KnownFact.all(:conditions => ['key like "domu_%%"']).each do |f|
+      if f.key =~ /domu_(\w+)$/
+        domu_name = $1
+        domus << self.find_by_name(domu_name)
+      end
+    end
+
+    domus
+  end
+
+  def self.find_by_name(name)
+    fact = KnownFact.current.find_by_key('domu_' + name)
+    return nil unless fact
+    json = ActiveSupport::JSON.decode(fact.value)
+    domU = DomUConfig.new(name)
+    domU.is_hvm = json['is_hvm']
+    domU.ram = json['ram']
+    domU.vcpus = json['vcpus']
+    domU.cpu_weight = json['cpu_weight']
+    domU.cpu_cap = json['cpu_cap']
+    domU.disks = json['disks'] # TODO
+    domU.eth0_mac = json['eth0_mac']
+    domU.eth1_mac = json['eth1_mac']
+    domU.vnc_port = json['vnc_port']
+    domU.boot_device = json['boot_device']
+    return domU
+  end
 end
 
 class GuestState
@@ -112,6 +143,3 @@ ActiveRecord::Base.establish_connection(
   :adapter => 'sqlite3',
   :database => "#{AGENT_ROOT}/databases/xen.sqlite"
 )
-
-class AgentState
-end
