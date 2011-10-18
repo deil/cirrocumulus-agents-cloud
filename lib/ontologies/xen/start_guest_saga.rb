b0927a2fd7fe7a2f1fcc20984320cced34173c1a
deil <deil@mneko.net>
2011-10-19 02:02:48 +0400

    xen: typo

--- lib/ontologies/xen/start_guest_saga.rb
+++ lib/ontologies/xen/start_guest_saga.rb
@@ -30,17 +30,17 @@ class StartGuestSaga < Saga
         end
 
         if XenNode.start_guest(guest) && XenNode.is_guest_running?(guest.name)
-          config = DomUConfig.new(guest_id)
-          config.is_hvm = guest_cfg[:is_hvm]
-          config.ram = guest_cfg[:ram]
-          config.vcpus = guest_cfg[:vcpus]
-          config.cpu_weight = guest_cfg[:cpu_weight]
-          config.cpu_cap = guest_cfg[:cpu_cap]
-          config.disks = guest_cfg[:disks]
-          config.eth0_mac = guest_cfg[:eth0_mac]
-          config.eth1_mac = guest_cfg[:eth1_mac]
-          config.vnc_port = guest_cfg[:vnc_port]
-          config.boot_device = guest_cfg[:network_boot] == 1 ? 'network' : 'hd'
+          config = DomUConfig.new(guest.name)
+          config.is_hvm = guest.type == :hvm
+          config.ram = guest.ram
+          config.vcpus = guest.vcpus
+          config.cpu_weight = guest.cpu_weight
+          config.cpu_cap = guest.cpu_cap
+          config.disks = guest.disks
+          config.eth0_mac = guest.eth0_mac
+          config.eth1_mac = guest.eth1_mac
+          config.vnc_port = guest.vnc_port
+          config.boot_device = gues.network_boot == 1 ? 'network' : 'hd'
           config.save('cirrocumulus', message.sender)
 
           notify_finished()
