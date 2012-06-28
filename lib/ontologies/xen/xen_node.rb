d0a06c788c62f5290372b49e225ab7fb2982bce6
Anton <deil@mneko.net>
2012-06-29 01:11:27 +0400

    xen: minor fix in XenNode

--- lib/ontologies/xen/xen_node.rb
+++ lib/ontologies/xen/xen_node.rb
@@ -149,7 +149,7 @@ class XenNode
   def self.perform_cmd(cmd, log_output = true)
     Log4r::Logger['agent'].debug "Executing command: #{cmd}" if log_output
     _, out, err = systemu(cmd)
-    Log4r::Logger['agent'].debug "Result: #{res.strip}" if !res.strip.blank? && log_output
+    Log4r::Logger['agent'].debug "Result: #{out.strip}" if !out.strip.blank? && log_output
     Log4r::Logger['agent'].debug "Error: #{err.strip}" if !err.strip.blank? && log_output
 
     err.blank?
