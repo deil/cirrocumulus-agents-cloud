cce0909914e7f2a699b29f00b6358c5e1da54d28
Anton <deil@mneko.net>
2012-12-16 18:52:07 +0400

    wip

--- lib/ontologies/hypervisor_ontology.rb
+++ lib/ontologies/hypervisor_ontology.rb
@@ -7,19 +7,12 @@ require_relative 'hypervisor/mdraid'
 class HypervisorOntology < Ontology
   ontology 'hypervisor'
 
-  rule 'init', [[:just_started]] do |ontology, params|
+  def restore_state
     Hypervisor.connect
     Hypervisor.set_cpu(0, 10000, 0)
 
-    ontology.collect_initial_guest_stats
-
-    ontology.retract [:just_started]
-  end
-
-  def restore_state
-    assert [:just_started]
-
     discover_new_disks
+    collect_initial_guest_stats
   end
 
   def handle_query(sender, expression, options = {})
@@ -42,25 +35,6 @@ class HypervisorOntology < Ontology
 
   end
 
-  def collect_initial_guest_stats
-    running_guests = Hypervisor.running_guests
-    running_guests.each do |guest_id|
-      guest = Hypervisor.find(guest_id)
-      debug "#{guest_id}"
-      debug "-> CPU = %.02f" % guest.cpu_time
-      assert [:guest, guest_id, :cpu_time, guest.cpu_time]
-
-      guest.interfaces.each_with_index do |vif, idx|
-        debug "-> vif#{idx} rx=%.02f tx=%.02f" % [vif[:rx]/(1024*1024), vif[:tx]/(1024*1024)]
-        assert [:guest, guest_id, :vif, idx, :rx, vif[:rx], :tx, vif[:tx]]
-      end
-
-      guest.block.each_key do |dev|
-        debug "-> #{dev} reads=%d (%.02f Mb) writes=%d (%.02f Mb)" % [guest.block[dev][:rd_req], guest.block[dev][:rd_bytes]/(1024*1024), guest.block[dev][:wr_req], guest.block[dev][:wr_bytes]/(1024*1024)]
-      end
-    end
-  end
-
   protected
 
   def discover_new_disks()
@@ -73,6 +47,22 @@ class HypervisorOntology < Ontology
       logger.info "autodiscovered virtual disk %d" % [discovered]
       disk = VirtualDisk.new(discovered)
       disk.save('discovered')
+    end
+  end
+
+  def collect_initial_guest_stats
+    running_guests = Hypervisor.running_guests
+    running_guests.each do |guest_id|
+      guest = Hypervisor.find(guest_id)
+      assert [:guest, guest_id, :cpu_time, guest.cpu_time]
+
+      guest.interfaces.each_with_index do |vif, idx|
+        assert [:guest, guest_id, :vif, idx, :rx, vif[:rx], :tx, vif[:tx]]
+      end
+
+      guest.block.each_key do |dev|
+        assert [:guest, guest_id, :block_device, dev, :reads, guest.block[dev][:rd_req], :rd_bytes, guest.block[dev][:rd_bytes], :writes, guest.block[dev][:wr_req], :wr_bytes, guest.block[dev][:wr_bytes]]
+      end
     end
   end
 
