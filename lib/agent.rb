df9201f21349c07c471ac3f3666a36e50b327f7c
Anton <deil@mneko.net>
2013-03-09 16:16:51 +0400

    updated log4r configuration

--- lib/agent.rb
+++ lib/agent.rb
@@ -1,27 +1,14 @@
 require 'bundler/setup'
+require 'log4r/configurator'
 require 'cirrocumulus'
 require 'cirrocumulus/remote_console'
 
-#require_relative 'ontologies/hypervisor_ontology'
-require_relative 'ontologies/cloud_ontology'
+require_relative 'ontologies/hypervisor_ontology'
+#require_relative 'ontologies/cloud_ontology'
 
 Encoding.default_internal = Encoding.default_external = 'UTF-8'
 
-agent_logger = Log4r::Logger.new('agent')
-agent_logger.outputters = Log4r::Outputter.stdout
-
-channels_logger = Log4r::Logger.new('channels')
-channels_logger.outputters = Log4r::Outputter.stdout
-
-jabber_logger = Log4r::Logger.new('channels::jabber')
-
-ontology_logger = Log4r::Logger.new('ontology')
-ontology_logger.outputters = Log4r::Outputter.stdout
-
-run_queue_logger = Log4r::Logger.new('ontology::run_queue')
-
-my_logger = Log4r::Logger.new('ontology::hypervisor')
-my_logger = Log4r::Logger.new('ontology::cloud')
+Log4r::Configurator.load_xml_file('config/log4r.xml')
 
 JabberChannel::server '172.16.11.4'
 JabberChannel::password 'q1w2e3r4'
@@ -30,7 +17,8 @@ JabberChannel::conference 'cirrocumulus'
 Ontology.enable_console
 
 agent = Cirrocumulus::Environment.new(`hostname`.chomp)
-agent.load_ontology(CloudOntology.new(Agent.network('cloud2')))
+#agent.load_ontology(CloudOntology.new(Agent.network('cloud2')))
+agent.load_ontology(HypervisorOntology.new(Agent.network('hypervisor')))
 agent.run
 gets
 agent.join
